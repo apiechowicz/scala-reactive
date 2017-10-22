1cb265dbd85b1dfd5185e0a20cb9334cca5f5d26
apiechowicz <andrzej.piechowicz.93@gmail.com>
2017-10-22 12:28:47 +0200

    [hw-lab2] Add skeletons for actors implementations

---
+++ src/main/scala/eShop/Cart.scala
@@ -0,0 +1,11 @@
+package eShop
+
+import akka.actor._
+
+class Cart extends Actor {
+  /*
+  states:
+  Empty, NonEmpty, InCheckout
+   */
+  override def receive = ???
+}
---
+++ src/main/scala/eShop/Checkout.scala
@@ -0,0 +1,11 @@
+package eShop
+
+import akka.actor.Actor
+
+class Checkout extends Actor {
+  /*
+  states:
+  Selecting, Delivery, Cancelled, SelectingPaymentMethod, ProcessingPayment, Closed
+  */
+  override def receive = ???
+}
