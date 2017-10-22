0fa66f754a1d32aa117ea41a36f3c1baa21e9e4d
apiechowicz <andrzej.piechowicz.93@gmail.com>
2017-10-22 13:57:41 +0200

    [hw-lab2] Add basic implementation of Cart states

--- src/main/scala/eShop/Cart.scala
+++ src/main/scala/eShop/Cart.scala
@@ -1,6 +1,9 @@
 package eShop
 
 import akka.actor._
+import akka.dispatch.sysmsg.Failed
+import akka.event.LoggingReceive
+import eShop.Cart._
 
 object Cart {
 
@@ -25,9 +28,33 @@ object Cart {
 }
 
 class Cart() extends Actor {
-  /*
-  states:
-  Empty, NonEmpty, InCheckout
-   */
-  override def receive = ???
+  var itemCount = BigDecimal(0)
+
+  override def receive: LoggingReceive = empty()
+
+  def empty(): LoggingReceive = {
+    case ItemAdded =>
+      itemCount += 1
+      context become nonEmpty
+    case _ => sender ! Failed
+  }
+
+  def nonEmpty(): LoggingReceive = {
+    case ItemAdded =>
+      itemCount += 1
+    case ItemRemoved =>
+      itemCount -= 1
+      if (itemCount == BigDecimal(0)) {
+        context become empty
+      }
+    case CheckoutStarted =>
+      context become inCheckout
+    case _ => sender ! Failed
+  }
+
+  def inCheckout(): LoggingReceive = {
+    case CheckoutClosed => context become empty
+    case CheckoutCancelled => context become nonEmpty
+    case _ => sender ! Failed
+  }
 }
