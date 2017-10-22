1b1631770e367c2fad2b513c1a6c75af28711e43
apiechowicz <andrzej.piechowicz.93@gmail.com>
2017-10-22 21:58:29 +0200

    [hw-lab2-fsm] Add Cart implemented as FSM (with timer)

---
+++ src/main/scala/eShop/Cart.scala
@@ -0,0 +1,76 @@
+package eShop
+
+import java.util.concurrent.TimeUnit
+
+import akka.actor.FSM
+
+import scala.concurrent.duration.FiniteDuration
+
+// events
+final case class ItemAdded()
+
+final case class ItemRemoved()
+
+final case class CartTimerExpired()
+
+final case class CheckoutStarted()
+
+final case class CheckoutCancelled()
+
+final case class CheckoutClosed()
+
+// states
+sealed trait State
+
+case object Empty extends State
+
+case object NonEmpty extends State
+
+case object InCheckout extends State
+
+sealed trait Data
+
+final case class ItemCount(value: Int) extends Data
+
+class Cart extends FSM[State, Data] {
+  val timerName = "CartTimerFSM"
+  val timerTimeout = FiniteDuration(30, TimeUnit.SECONDS)
+
+  startWith(Empty, ItemCount(0))
+
+  when(Empty) {
+    case Event(ItemAdded, _) => goto(NonEmpty) using ItemCount(1)
+  }
+
+  when(NonEmpty) {
+    case Event(ItemAdded, _) => stay using ItemCount(stateData.value + 1)
+    case Event(ItemRemoved, _) =>
+      if (stateData.value > 1) {
+        stay using ItemCount(stateData.value - 1)
+      } else {
+        goto(Empty) using ItemCount(0)
+      }
+    case Event(CheckoutStarted, _) => goto(InCheckout) using stateData
+  }
+
+  when(InCheckout) {
+    case Event(CheckoutClosed, _) => goto(Empty) using ItemCount(0)
+    case Event(CheckoutCancelled, _) => goto(NonEmpty) using stateData
+  }
+
+  onTransition {
+    case _ -> NonEmpty => setTimer(timerName, CartTimerExpired, timerTimeout, repeat = false)
+    case NonEmpty -> _ => cancelTimer(timerName)
+  }
+
+  whenUnhandled {
+    case Event(CartTimerExpired, _) =>
+      if (stateName == NonEmpty) {
+        goto(Empty) using ItemCount(0)
+      } else {
+        stay using stateData
+      }
+  }
+
+  initialize()
+}
