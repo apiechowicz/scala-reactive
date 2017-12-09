b6f5196711adab68be6de880ebe61e84155e6ae1
apiechowicz <andrzej.piechowicz.93@gmail.com>
2017-12-09 22:28:23 +0100

    Create ProductCatalog

--- src/main/scala/catalog/ProductParser.scala
+++ src/main/scala/catalog/ProductParser.scala
@@ -26,7 +26,7 @@ object ProductParser {
 
   private def parseName(productInfo: Array[String]): String = {
     val name = new StringBuilder(productInfo(1).trim)
-    if (productInfo(2) != "NULL")
+    if (productInfo.length >= 3 && productInfo(2) != "NULL")
       name.append(" " + productInfo(2).trim)
     name.toString()
   }
